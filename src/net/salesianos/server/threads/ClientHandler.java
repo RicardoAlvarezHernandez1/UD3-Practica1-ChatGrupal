24e27a4d90c2692d70b1f1579d579c6e68ddd65d
ravel01106 <amandaravelocabrera@gmail.com>
2024-01-17 13:53:42 +0000

    refactor: deleted an unused import

--- src/net/salesianos/server/threads/ClientHandler.java
+++ src/net/salesianos/server/threads/ClientHandler.java
@@ -2,7 +2,6 @@ package net.salesianos.server.threads;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.util.ArrayList;
 
